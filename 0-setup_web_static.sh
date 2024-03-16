333ded894b8a2865d42f352ee6c86fcd6c08d221
2005-annabel <owangeshi@gmail.com>
2024-03-16 14:33:40 -0700

    2

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#The script setsup my web servers for the deployment of web-static
+    sudo apt-get update
+    sudo apt-get install -y nginx
+    sudo ufw allow 'Nginx HTTP'
+#creates the specified folders if they don't already exist
+
+    sudo mkdir -p "/data"
+
+    sudo mkdir -p "/data/web_static/"
+
+    sudo mkdir -p "/data/web_static/releases/"
+
+    sudo mkdir -p "/data/web_static/shared/"
+
+    sudo mkdir -p "/data/web_static/releases/test/"
+
+    sudo touch /data/web_static/releases/test/index.html
+#creates a simple html file for testing purposes
+sudo echo "<html>
+<head>
+</head>
+<body>
+    Hoberton School
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+#Giving ownership of the '/data' folder to 'Ubuntu'(group and user)
+sudo chown -R ubuntu:ubuntu /data/
+#updating the Nginx configuration to serve the content of '/data/web_static/current/' to 'hbnb_static'
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service restart nginx
+
+
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A fabric script that distributes archive
+   to the web servers
+"""
+from fabric.api import *
+from os.path import exists
+env.hosts = ['	54.237.80.147', '100.24.235.237']
+
+
+def do_deploy(archive_path):
+    """deploys web_static
+    """
+    if exists(archive_path) is False:
+        return False
+        archive_filename = archive_path.split('/')[-1]
+        archive_folder = '/data/web_static/releases/{}'.format(
+            archive_filename.split('.')[0])
+    try:
+        put(archive_path, "/tmp/")
+        run('sudo mkdir -p {}./'.format(archive_folder))
+        run('sudo tar -xzf /tmp/{} -C {}/'.format(
+            archive_filename, archive_folder))
+        run('sudo rm /tmp/{}'.format(archive_filename))
+        run("sudo mv {}/web_static/* {}/".format(
+            archive_folder, archive_folder))
+        run("sudo rm -rf {}/web_static".format(archive_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(archive_folder))
+        return True
+    except Exception:
+        return False
